e5e3a4a25895eb11edb91eeb3972862dcd625a4a
Elijah Chia <elcxd99@gmail.com>
2024-08-07 20:26:01 +0800

    modified golf stats gen. scripts to generate data in a format compatible with data analyser

--- DataGeneratorScripts/golfstatsgen_handicap20.py
+++ DataGeneratorScripts/golfstatsgen_handicap20.py
@@ -5,98 +5,39 @@ CLUBS = ["DRIVER", "3 Wood", "5 Wood", "4 Iron", "5 Iron", "6 Iron", "7 Iron", "
 CLUB_DISTS = [225, 185, 165, 160, 155, 145, 135, 125, 115, 85, 70, 55, 10]
 LIES = ["TEE BOX", "FAIRWAY", "FAIRWAY", "FAIRWAY", "FAIRWAY", "ROUGH", "ROUGH", "ROUGH", "BUNKER", "OTHER/HAZARD", "GREEN"]
 
-PAR_3_MAX = 240
-PAR_3_MIN = 100
+PAR_3_MAX, PAR_3_MIN = 240, 100
+PAR_4_MAX, PAR_4_MIN = 450, 280
+PAR_5_MAX, PAR_5_MIN = 580, 450
 
-PAR_4_MAX = 450
-PAR_4_MIN = 280
+data, distances, clubs, lies = [], [], [], []
 
-PAR_5_MAX = 580
-PAR_5_MIN = 450
+randClub, strokes, totalStrokes = 0, 0, 0
 
-data = []
-distances = []
-clubs = []
-lies = []
-
-randClub = 0
-strokes = 0
-totalStrokes = 0
-
-HOLE = ['HOLE', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+HOLE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
         11, 12, 13, 14, 15, 16, 17, 18]
 
-PAR = ['PAR'] 
 PARS = [3, 3, 3, 3, 
        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
        5, 5, 5, 5]
 
-YARDAGE = ['YARDAGE']
-STROKES = ['STROKES']
+YARDAGE, STROKES = [], []
 
-STROKE_1_DISTANCE = ['STROKE_1_DISTANCE']
-STROKE_1_CLUB = ['STROKE_1_CLUB']
-STROKE_1_LIE = ['STROKE_1_LIE']
-
-STROKE_2_DISTANCE = ['STROKE_2_DISTANCE']
-STROKE_2_CLUB = ['STROKE_2_CLUB']
-STROKE_2_LIE = ['STROKE_2_LIE']
-
-STROKE_3_DISTANCE = ['STROKE_3_DISTANCE']
-STROKE_3_CLUB = ['STROKE_3_CLUB']
-STROKE_3_LIE = ['STROKE_3_LIE']
-
-STROKE_4_DISTANCE = ['STROKE_4_DISTANCE']
-STROKE_4_CLUB = ['STROKE_4_CLUB']
-STROKE_4_LIE = ['STROKE_4_LIE']
-
-STROKE_5_DISTANCE = ['STROKE_5_DISTANCE']
-STROKE_5_CLUB = ['STROKE_5_CLUB']
-STROKE_5_LIE = ['STROKE_5_LIE']
-
-STROKE_6_DISTANCE = ['STROKE_6_DISTANCE']
-STROKE_6_CLUB = ['STROKE_6_CLUB']
-STROKE_6_LIE = ['STROKE_6_LIE']
-
-STROKE_7_DISTANCE = ['STROKE_7_DISTANCE']
-STROKE_7_CLUB = ['STROKE_7_CLUB']
-STROKE_7_LIE = ['STROKE_7_LIE']
-
-STROKE_8_DISTANCE = ['STROKE_8_DISTANCE']
-STROKE_8_CLUB = ['STROKE_8_CLUB']
-STROKE_8_LIE = ['STROKE_8_LIE']
-
-STROKE_9_DISTANCE = ['STROKE_9_DISTANCE']
-STROKE_9_CLUB = ['STROKE_9_CLUB']
-STROKE_9_LIE = ['STROKE_9_LIE']
-
-STROKE_10_DISTANCE = ['STROKE_10_DISTANCE']
-STROKE_10_CLUB = ['STROKE_10_CLUB']
-STROKE_10_LIE = ['STROKE_10_LIE']
+STROKE_1_DISTANCE, STROKE_1_CLUB, STROKE_1_LIE = [], [], []
+STROKE_2_DISTANCE, STROKE_2_CLUB, STROKE_2_LIE = [], [], []
+STROKE_3_DISTANCE, STROKE_3_CLUB, STROKE_3_LIE = [], [], []
+STROKE_4_DISTANCE, STROKE_4_CLUB, STROKE_4_LIE = [], [], []
+STROKE_5_DISTANCE, STROKE_5_CLUB, STROKE_5_LIE = [], [], []
+STROKE_6_DISTANCE, STROKE_6_CLUB, STROKE_6_LIE = [], [], []
+STROKE_7_DISTANCE, STROKE_7_CLUB, STROKE_7_LIE = [], [], []
+STROKE_8_DISTANCE, STROKE_8_CLUB, STROKE_8_LIE = [], [], []
+STROKE_9_DISTANCE, STROKE_9_CLUB, STROKE_9_LIE = [], [], []
+STROKE_10_DISTANCE, STROKE_10_CLUB, STROKE_10_LIE = [], [], []
 
 # shuffle par order for each hole
 random.shuffle(PARS)
 
-# Add pars to par list
-for i in range(len(PARS)):
-    PAR.append(PARS[i])
-
-# added HOLE and PAR lists to the main data list
-data.extend([HOLE, PAR, YARDAGE, STROKES, 
-             STROKE_1_DISTANCE, STROKE_1_CLUB, STROKE_1_LIE, 
-             STROKE_2_DISTANCE, STROKE_2_CLUB, STROKE_2_LIE,
-             STROKE_3_DISTANCE, STROKE_3_CLUB, STROKE_3_LIE,
-             STROKE_4_DISTANCE, STROKE_4_CLUB, STROKE_4_LIE,
-             STROKE_5_DISTANCE, STROKE_5_CLUB, STROKE_5_LIE,
-             STROKE_6_DISTANCE, STROKE_6_CLUB, STROKE_6_LIE,
-             STROKE_7_DISTANCE, STROKE_7_CLUB, STROKE_7_LIE,
-             STROKE_8_DISTANCE, STROKE_8_CLUB, STROKE_8_LIE,
-             STROKE_9_DISTANCE, STROKE_9_CLUB, STROKE_9_LIE,
-             STROKE_10_DISTANCE, STROKE_10_CLUB, STROKE_10_LIE])
-
 # simulate golf game for all 18 holes
 for i in range(18):
-    
     if PARS[i] == 3:
         yardage = int(random.randint(PAR_3_MIN, PAR_3_MAX))
     if PARS[i] == 4:
@@ -111,28 +52,28 @@ for i in range(18):
         
         if dist2Go > 240 and strokes == 0:
             randClub = 0
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif dist2Go > 200:
             randClub = random.randint(1, 2)
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif 200 >= dist2Go > 180:
             randClub = int(random.randint(3, 4))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif 180 >= dist2Go > 150:
             randClub = int(random.randint(5, 6))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif 150 >= dist2Go > 120:
             randClub = int(random.randint(7, 8))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif 120 >= dist2Go > 90:
             randClub = int(random.randint(9, 10))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 20, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         elif 90 >= dist2Go > 50:
             randClub = int(random.randint(11, 12))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 10, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
         else:
             randClub = int(random.randint(11, 12))
-            strokeDist = random.randint(CLUB_DISTS[randClub] - 10, CLUB_DISTS[randClub] + 5)
+            strokeDist = random.randint(CLUB_DISTS[randClub] - 5, CLUB_DISTS[randClub] + 5)
 
         if dist2Go - strokeDist < 0:
             overShot = dist2Go - strokeDist
@@ -160,7 +101,7 @@ for i in range(18):
 
         if dist2Go >= 10:
             puttDist = dist2Go - random.randint(1, 3)
-        elif dist2Go > 2:
+        elif dist2Go > 3:
             puttDist = dist2Go - random.randint(0, 2)
         else:
             puttDist = dist2Go
@@ -232,7 +173,7 @@ for i in range(18):
 
     totalStrokes += strokes
     
-    print("PAR:", PARS[i],"STROKES:", strokes,"YARDAGE:", yardage)
+    #print("PAR:", PARS[i],"STROKES:", strokes,"YARDAGE:", yardage)
     
     strokes = 0
 
@@ -240,13 +181,31 @@ for i in range(18):
     clubs.clear()
     lies.clear()
 
+tuples = list(zip(HOLE, PARS, YARDAGE, STROKES, STROKE_1_DISTANCE, STROKE_1_CLUB, STROKE_1_LIE, 
+                  STROKE_2_DISTANCE, STROKE_2_CLUB, STROKE_2_LIE, 
+                  STROKE_3_DISTANCE, STROKE_3_CLUB, STROKE_3_LIE, 
+                  STROKE_4_DISTANCE, STROKE_4_CLUB, STROKE_4_LIE, 
+                  STROKE_5_DISTANCE, STROKE_5_CLUB, STROKE_5_LIE, 
+                  STROKE_6_DISTANCE, STROKE_6_CLUB, STROKE_6_LIE, 
+                  STROKE_7_DISTANCE, STROKE_7_CLUB, STROKE_7_LIE, 
+                  STROKE_8_DISTANCE, STROKE_8_CLUB, STROKE_8_LIE, 
+                  STROKE_9_DISTANCE, STROKE_9_CLUB, STROKE_9_LIE, 
+                  STROKE_10_DISTANCE, STROKE_10_CLUB, STROKE_10_LIE))
+
 # format, print and save data into a .csv file
-df = pd.DataFrame(data)
+df = pd.DataFrame(tuples, columns = ['HOLE', 'PARS', 'YARDAGE', 'STROKES', 'STROKE_1_DISTANCE', 'STROKE_1_CLUB', 'STROKE_1_LIE', 
+                  'STROKE_2_DISTANCE', 'STROKE_2_CLUB', 'STROKE_2_LIE', 
+                  'STROKE_3_DISTANCE', 'STROKE_3_CLUB', 'STROKE_3_LIE', 
+                  'STROKE_4_DISTANCE', 'STROKE_4_CLUB', 'STROKE_4_LIE', 
+                  'STROKE_5_DISTANCE', 'STROKE_5_CLUB', 'STROKE_5_LIE', 
+                  'STROKE_6_DISTANCE', 'STROKE_6_CLUB', 'STROKE_6_LIE', 
+                  'STROKE_7_DISTANCE', 'STROKE_7_CLUB', 'STROKE_7_LIE', 
+                  'STROKE_8_DISTANCE', 'STROKE_8_CLUB', 'STROKE_8_LIE', 
+                  'STROKE_9_DISTANCE', 'STROKE_9_CLUB', 'STROKE_9_LIE', 
+                  'STROKE_10_DISTANCE', 'STROKE_10_CLUB', 'STROKE_10_LIE'])
+
 print(df)
 
-df.to_csv('golf_stats_handicap20.csv', index=False)
+df.to_csv('golf_stats_handicap0.csv', index=False)
 
 print("TOTAL PAR: 72", "TOTAL STROKES: ", totalStrokes)
-
-
-
